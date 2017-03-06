ea7484743edbc855a605b8a5defaa426256d159c
Atanas Kambitov <atanasskambitovv@gmail.com>
2017-03-06 18:44:48 +0200

    Average Grades

---
+++ Prgramming fundamentals/9.ObjectsAndClassesExercises/AverageGrades/AverageGrades.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageGrades
+{
+    class AverageGrades
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedDictionary<string, double> QualifiedStudents = new SortedDictionary<string, double>();
+            Student students = new Student();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(' ').ToArray();
+
+                List<double> GradesList = new List<double>();
+
+                students.Name = input[0];
+             //   if (QualifiedStudents.Contains(students.Name)) { QualifiedStudents.Add(students.Name, ); }
+
+                for (int j = 1; j < input.Length; j++)
+                {
+                    double gradeToDouble = double.Parse(input[j]);
+                    if(gradeToDouble > 6)
+                        gradeToDouble  = gradeToDouble / 100;
+
+                    GradesList.Add(gradeToDouble);
+                }
+                students.Grades = GradesList;
+
+                double average = students.AverageGrade();
+
+                if(average >= 5)
+                {
+                    
+
+                    QualifiedStudents[students.Name] = average;
+                }
+
+
+            }
+
+            foreach (var qualifiedStudent in QualifiedStudents)
+            {
+                Console.WriteLine("{0} -> {1:f2}", qualifiedStudent.Key, qualifiedStudent.Value);
+            }
+
+        }
+    }
+}
---
+++ Prgramming fundamentals/9.ObjectsAndClassesExercises/AverageGrades/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageGrades
+{
+    class Student
+    {
+        public string Name { set; get; }
+        public List<double> Grades { set; get; }
+        
+        public double AverageGrade() {
+
+            return Grades.Average();
+                }  // readOnly
+
+    }
+}
