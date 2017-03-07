6f673c073d9da6b6d4a22c784ec3cbb5c43cb037
Atanas Kambitov <atanasskambitovv@gmail.com>
2017-03-07 20:58:16 +0200

    mentor update

--- Prgramming fundamentals/9.ObjectsAndClassesExercises/Mentor/Mentor.cs
+++ Prgramming fundamentals/9.ObjectsAndClassesExercises/Mentor/Mentor.cs
@@ -12,16 +12,23 @@ namespace Mentor
         static void Main(string[] args)
         {
             Dictionary<string, Student> Students = new Dictionary<string, Student>();       
-            Dictionary<string, List<DateTime>> NamesAndDates = new Dictionary< string, List<DateTime>>();
-            Dictionary<string, List<string>> NamesAndComments = new Dictionary<string, List<string>>();
+          
+			
+			/*
+			nakov 22/08/2016,20/08/2016
+			simeon10 22/08/2016
+			end of dates
+			nakov-Excellent algorithmetic thinking.
+			Gesh4o-Total noob.
+			end of comments
+			*/
 
-            string namesAndDates = Console.ReadLine();
-            GetNamesAndDates(namesAndDates, NamesAndDates);
+			string namesAndDates = Console.ReadLine();
+            GetNamesAndDates(namesAndDates, Students);
 
             string namesAndComments = Console.ReadLine();
-            GetNamesAndComments(namesAndComments, NamesAndComments, NamesAndDates);
+            GetNamesAndComments(namesAndComments, Students);
 
-            SetStudent(NamesAndDates, NamesAndComments, Students);
 
             PrintResult(Students);
         
@@ -29,15 +36,19 @@ namespace Mentor
 
         private static void PrintResult(Dictionary<string, Student> Students)
         {
-            foreach (var student in Students)
+            foreach (var student in Students.OrderBy(x => x.Key))
             {
                 Console.WriteLine(student.Key);
                 Console.WriteLine("Comments:");
-                foreach (var comment in student.Value.Comments.OrderBy(c => c))
-                {
-                    Console.WriteLine("- {0}", comment);
-                }
-                Console.WriteLine("Dates attended:");
+				if (!student.Value.Comments.Contains(null)); // ako broikata na kometarite ne e 0
+				{// PROBLEMA E TUKA
+					foreach (var comment in student.Value.Comments)
+					{
+						Console.WriteLine("- {0}", comment);
+					}
+				}
+
+				Console.WriteLine("Dates attended:");
                 foreach (var date in student.Value.Dates.OrderBy(d => d))
                 {
                     Console.WriteLine("-- {0}", date.ToString("dd/MM/yyyy"));
@@ -46,83 +57,65 @@ namespace Mentor
             }
         }
 
-        private static void SetStudent(Dictionary<string, List<DateTime>> NamesAndDates,
-            Dictionary<string, List<string>> NamesAndComments,
-            Dictionary<string, Student> Students)
-        {
-            foreach (var pair in NamesAndDates)
-            {
-                string name = pair.Key;
-                List<DateTime> date = pair.Value;
-                List<string> comment = new List<string>();
-                foreach (var pair2 in NamesAndComments)
-                {
-                    if (pair.Key == pair2.Key)
-                    {
-                        comment = pair2.Value;
-                    }
-                }
-
-                // List<string> comment = NamesAndComments.
-
-                Student student = new Student()
-                {
-                    Name = name,
-                    Dates = date,
-                    Comments = comment,
-
-                };
-
-
-                Students[name] = student;
-
-
-            }
-        }
 
         private static void GetNamesAndComments(string namesAndComments,
-            Dictionary<string, List<string>> NamesAndComments,
-            Dictionary<string, List<DateTime>> NamesAndDates)
-        {
+			Dictionary<string, Student> Students)
+		{
             
             while (!namesAndComments.Equals("end of comments"))
             {
-                List<string> comment = new List<string>();
+                List<string> comments = new List<string>();
                 string[] namesAndCommentsArr = namesAndComments
                     .Split('-').ToArray();
                 string name = namesAndCommentsArr[0];
-                comment.Add( namesAndCommentsArr[1]);
 
-                if (NamesAndDates.ContainsKey(name))
-                {
-                    // ako toq student nqma data, ne go zapisvai
-                    NamesAndComments[name] = comment;
-                }
+				if (!comments.Contains(namesAndCommentsArr[1]))
+				{
+					comments.Add(namesAndCommentsArr[1]);
+					
+					
+				}
+
+
+				foreach (var student in Students.Values)
+				{
+					if (student.Name.Equals(name))
+					{
+						student.Comments = comments;
+					}
+				}
+                
 
 
                 namesAndComments = Console.ReadLine();
             }
         }
 
-        private static void GetNamesAndDates(string namesAndDates,
-            Dictionary<string, List<DateTime>> NamesAndDates)
+        private static void GetNamesAndDates(string namesAndDates, 
+			Dictionary<string, Student> Students)
         {
-            while (!namesAndDates.Equals("end of dates"))
-            {
-                List<DateTime> dates = new List<DateTime>();
-                string[] namesAndDatesArr = namesAndDates.Split(' ').ToArray();
-                string name = namesAndDatesArr[0];
 
-                string[] datesArr = namesAndDatesArr[1].Split(',').ToArray();
-                for (int i = 0; i < datesArr.Length; i++)
-                {
-                    DateTime date = DateTime.ParseExact(datesArr[i],
-                        "dd/MM/yyyy", CultureInfo.InvariantCulture);
+			while (!namesAndDates.Equals("end of dates"))
+			{
+				List<DateTime> dates = new List<DateTime>();
 
-                    dates.Add(date);
-                }
+				Student student = new Student();
 
-                NamesAndDates[name] = dates;
+				string[] namesAndDatesArr = namesAndDates.Split(' ').ToArray();
+				string name = namesAndDatesArr[0];
+
+				string[] datesArr = namesAndDatesArr[1].Split(',').ToArray();
+				for (int i = 0; i < datesArr.Length; i++)
+				{
+					DateTime date = DateTime.ParseExact(datesArr[i],
+						"dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+					if (!dates.Contains(date)) { dates.Add(date); }   // ako q nqma taq data q sloji
+				}
+
+				student.Name = name;
+				student.Dates = dates;
+				Students[name] = student;
                 namesAndDates = Console.ReadLine();
             }
         }
