01471958509814b0213fc58ae79ef8aa75ff5033
Atanas Kambitov <atanasskambitov@gmail.com>
2017-03-28 02:01:10 +0300

    nmmb

---
+++ 3.Software Technologies/1.CourseOpening/CourseOpening/CourseOpening.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseOpening
+{
+    class CourseOpening
+    {
+        static void Main(string[] args)
+        {
+            /*
+             zapochvame s html5 i css3,
+             ima 4 tehnologii:
+             1. Web development PHP    shte pravim prilojenie
+             2. Web development JavaSript    polzvano e za fromt end no dnes moje da se polzva za pochti vsichko
+             3. Web development C#       web formi, frameworkove, prilojeniq
+             
+            Shte vdigame prilojeniq sus vsichki tezi ezici
+
+            NQMA DA STANEM PROFESIONALISTI SLED TOZI KURS.
+            TOZI KURS  SAMO ZA ZAPOZNANSTVO S TEZI TEHNOLOGII
+
+            MOJEM DA SI IZBEREM POVECHE OT EDNO ZA NAPRED !!! 
+
+            V purvata chast na kursa shte ima PHP i  MySQL
+            shte pishem na  PHP 7, bazata shte bude na MySQL
+            , Frameworka shte bude na Doctrine i Symfony
+
+
+            Vuv Vtorata chast shte polzvame JavaScript i bazata shte bude na MongoDB
+            Mojem da pishem backenda na Node.Js ili Express.js  a za frontEnd shte pishem na 
+            ES6 
+
+
+            V tretata chast shte napravim sushtoto no sus Java 8, kato shte vdignem prilojenieto 
+            na Spring MVS kato framework  i  Hibernate kato logikata bazata i dr.
+
+
+            V chetvurtata chast na kursa shte pishem na C# 6 kato polzvame za framework EntityFramework
+            i ASP.Net MVC ,  a logikata i bazata shte gi napravim na SQL SERVER
+
+
+
+            SHTE POLUCHIM BEZPLTEN LICENZ ZA 
+            WebStorm - koeto e za JavaScript,     svalq se ot jetbrains.com/webstorm
+            PhpStorm - koeto e za PHP,            svalq se ot jetbrains.com/phpstorm
+            Visual Studio - koeto e za C#,        svalq se ot visulstudio.com
+            Intelli J Idea - koeto e za Java      svalq se ot jetbrains.com/idea
+
+             
+             */
+        }
+    }
+}
