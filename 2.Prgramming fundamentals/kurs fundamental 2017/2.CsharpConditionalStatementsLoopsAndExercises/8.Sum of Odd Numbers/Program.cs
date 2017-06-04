e734ee4b451123fc00efed59ef63ae33f2fcdb40
Atanas Kambitov <atanasskambitovv@gmail.com>
2017-06-04 16:51:39 +0300

    xxddd

---
+++ 2.Prgramming fundamentals/kurs fundamental 2017/2.CsharpConditionalStatementsLoopsAndExercises/8.Sum of Odd Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Sum_of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int n = 1;
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine(n);
+                n = n + 2;
+            }
+            Console.WriteLine("Sum: {0}", num*num);
+        }
+    }
+}
