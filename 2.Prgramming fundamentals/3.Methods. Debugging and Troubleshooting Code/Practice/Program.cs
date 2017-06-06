4654a427a564e586162599f1662ccab945e40dc8
Atanas Kambitov <atanasskambitovv@gmail.com>
2017-06-06 20:33:59 +0300

    iyhkjuhn

---
+++ 2.Prgramming fundamentals/3.Methods. Debugging and Troubleshooting Code/Practice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+       
+        
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double result = GetTriangleArea(width, height);
+            Console.WriteLine(result);
+        }
+
+        private static double GetTriangleArea(double width, double height)
+        {
+            double area = (width * height) / 2.0;
+            return area; 
+        }
+    }
+}
