c4b7c1411ad11182b031619343f586eba2ab82c2
Atanas Kambitov <atanasskambitovv@gmail.com>
2017-06-13 05:19:01 +0300

    juhgfd

---
+++ 2.Prgramming fundamentals/kurs fundamental 2017/5.CsharpArrayAndMethodsMoreExersices/06.Heists/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Heists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] priceOfJewelsAndGold = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int jewelsPrice = priceOfJewelsAndGold[0];
+            int goldPrice = priceOfJewelsAndGold[1];
+
+            string[] command = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            int totalEarnings = 0;
+            int totalExpences = 0;
+
+            while (command[0] != "Jail" && command[1] != "Time")
+            {
+                int countOfJewels = 0;
+                int countOfGold = 0;
+                int expences = int.Parse(command[1]);
+
+                totalExpences += expences;
+
+                foreach (var item in command[0].ToString())
+                {
+                    if (item == '%')
+                        countOfJewels++;
+                    else if (item == '$')
+                        countOfGold++;
+                }
+
+                int jewelsEarnings = countOfJewels * jewelsPrice;
+                int goldEarnings = countOfGold * goldPrice;
+                int earnings = jewelsEarnings + goldEarnings;
+                totalEarnings += earnings;
+
+
+                command = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            }
+
+            int moneyEarned = 0;
+            int moneyLost = 0;
+
+            if (totalEarnings >= totalExpences)
+            {
+                moneyEarned = totalEarnings - totalExpences;
+                Console.WriteLine($"Heists will continue. Total earnings: {moneyEarned}.");
+            }
+            else
+            {
+                moneyLost = totalExpences - totalEarnings;
+                Console.WriteLine($"Have to find another job. Lost: {moneyLost}.");
+            }
+        }
+    }
+}
---
+++ 2.Prgramming fundamentals/kurs fundamental 2017/5.CsharpArrayAndMethodsMoreExersices/07.Inventory Matcher/_07.Inventory_Matcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Inventory_Matcher
+{
+    class Inventory_Matcher
+    {
+        static void Main(string[] args)
+        {
+            string[] namesOfProducts = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            long[] quantitiesOfProducts = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToArray();
+
+            decimal[] pricesOfProducts = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .ToArray();
+
+            string command = Console.ReadLine();
+            while (command != "done")
+            {
+                int index = Array.IndexOf(namesOfProducts, command);
+                string name = namesOfProducts[index];
+                decimal price = pricesOfProducts[index];
+                long quantity = quantitiesOfProducts[index];
+
+                Console.WriteLine($"{name} costs: {price}; Available quantity: {quantity}");
+
+                command = Console.ReadLine();
+            }
+
+        }
+    }
+}
