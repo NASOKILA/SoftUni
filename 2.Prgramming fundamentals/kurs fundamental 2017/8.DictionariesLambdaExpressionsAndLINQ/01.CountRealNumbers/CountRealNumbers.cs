613eeaf40819c4a7c4dfb386c702031ce1eb4ec3
Atanas Kambitov <atanasskambitovv@gmail.com>
2017-06-16 03:47:11 +0300

    srhdrhjdrgrdg

---
+++ 2.Prgramming fundamentals/kurs fundamental 2017/8.DictionariesLambdaExpressionsAndLINQ/01.CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountRealNumbers
+{
+    class CountRealNumbers
+    {
+        static void Main(string[] args)
+        {
+
+            List<double> nums = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            foreach (var item in nums)
+            {
+                if (!counts.ContainsKey(item))
+                    counts[item] = 1;
+                else
+                    counts[item]++;
+            }
+
+            foreach (var key in counts.Keys)
+            {
+                Console.WriteLine($"{key} -> {counts[key]}");
+            }
+        }
+    }
+}
---
+++ 2.Prgramming fundamentals/kurs fundamental 2017/8.DictionariesLambdaExpressionsAndLINQ/04.LargestThreeNumbers/LargestThreeNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.LargestThreeNumbers
+{
+    class LargestThreeNumbers
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            Console.WriteLine(string.Join(" ",nums.OrderByDescending(n => n).Take(3).ToList()));
+
+        }
+    }
+}
