59a5cbeb841159cbe7b9795ceedcb6a96e3c7330
Atanas Kambitov <atanasskambitov@gmail.com>
2017-06-27 22:40:20 +0300

    adjxksl

---
+++ 2.Prgramming fundamentals/kurs fundamental 2017/15.StringAndTextProcessing/StringAndTextProcessing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace StringAndTextProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
