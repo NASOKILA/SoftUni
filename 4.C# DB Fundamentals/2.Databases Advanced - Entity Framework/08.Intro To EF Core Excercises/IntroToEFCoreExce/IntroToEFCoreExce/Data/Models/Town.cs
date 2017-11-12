eecdd993b5207e310832bc96d465081e80a8705f
Atanas Kambitov <atanasskambitov@gmail.com>
2017-11-12 08:03:59 +0200

    olui

---
+++ 4.C# DB Fundamentals/2.Databases Advanced - Entity Framework/08.Intro To EF Core Excercises/IntroToEFCoreExce/IntroToEFCoreExce/Data/Models/Town.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P02_DatabaseFirst.Data.Models
+{
+    public class Town
+    {
+        public Town()
+        {
+        }
+
+        public int TownId { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Address> Addresses { get; set; }
+        = new HashSet<Address>();
+    }
+}
---
+++ 4.C# DB Fundamentals/2.Databases Advanced - Entity Framework/08.Intro To EF Core Excercises/IntroToEFCoreExce/IntroToEFCoreExce/Program.cs
@@ -0,0 +1,37 @@
+﻿using P02_DatabaseFirst.Data;
+using P02_DatabaseFirst.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace P02_DatabaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SoftUniContext())
+            {
+                
+             /*ZADACHITE GI IMAM VECHE VUV FAILOVE !
+                 
+            Comments:
+                KATO POLZVAME .Include() NIE VZIMAME VSICHKO OT TABLICATA KOQTO INKLUVAME !!!
+                TOVA PRAVI PROCESA PO BAVEN, PO DOBRE E DA VZIMAME SAMO TOVA KOETO NI E NUJNO.
+
+                Po dobre e da polzvame anonimni obekti vus Select()  :
+                    Primer:
+                    .Select( a => new {
+
+                        //OT TUK SI VZZIMME SAMO TOVA KOETO NI TRQBVA !!!
+                    })
+
+             
+             */
+
+            }
+        }
+    }
+}
