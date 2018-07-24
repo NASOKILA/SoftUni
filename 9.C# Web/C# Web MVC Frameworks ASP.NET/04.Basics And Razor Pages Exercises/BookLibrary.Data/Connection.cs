afc5469ce750c02480c8cc05fb7aec8bd8ce92f5
Atanas Kambitov <atanasskambitov@gmail.com>
2018-07-25 02:55:19 +0300

    Security vulnerabilities

---
+++ 9.C# Web/C# Web MVC Frameworks ASP.NET/04.Basics And Razor Pages Exercises/BookLibrary.Data/Connection.cs
@@ -0,0 +1,7 @@
+﻿namespace BookLibrary.Data
+{
+    public static class Connection
+    {
+        public static string ConnectionString { get; set; } = "Data Source=HAL\\MSSQLSERVER2;Database=RazorBookLibrary;Integrated Security=True";
+    }
+}
---
+++ 9.C# Web/C# Web MVC Frameworks ASP.NET/04.Basics And Razor Pages Exercises/BookLibrary.Web/Pages/Models/BookIndexModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BookLibrary.Web.Pages.Models
+{
+    public class BookIndexModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        
+        public BookLibrary.Models.Author Author { get; set; }
+        
+        public string Status { get; set; }
+    }
+}
