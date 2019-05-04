0202ad2afa915138264f42c1592589660e96806c
NASOKILA <atanasskambitovv@gmail.com>
2019-05-04 17:21:24 +0300

    CV Latest Uptate

--- 9.C# Web/C# Web MVC Frameworks ASP.NET/21.ASP.NET WebAPI Exercises/SoftUniClone.Web/Startup.cs
+++ 9.C# Web/C# Web MVC Frameworks ASP.NET/21.ASP.NET WebAPI Exercises/SoftUniClone.Web/Startup.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿    using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
